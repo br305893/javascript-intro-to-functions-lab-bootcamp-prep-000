f27a0e3a23886fbe6f349ef2895f4e86568a3c8b
Ben Rosner <br305893@gmail.com>
2018-05-13 23:51:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(uppercase) {
+  var uppercase = "hello"
+  console.log(uppercase.toUpperCase)
+}
