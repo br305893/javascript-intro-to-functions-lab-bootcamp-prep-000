3230fcdb58c55fc88454945b733af98a327f4f6e
Ben Rosner <br305893@gmail.com>
2018-05-13 23:56:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  var string = "HELLO!"
+  var string = "HELLO"
   string.toUpperCase() === string
   console.log(string)
   }
 function logWhisper(string) {
-  var string = "hello!"
+  var string = "hello"
   string.toLowerCase() === string
   console.log(string)
 }
