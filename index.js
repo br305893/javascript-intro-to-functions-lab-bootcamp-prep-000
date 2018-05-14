57d9189a60325732608ba56878e470ed434c7e9b
Ben Rosner <br305893@gmail.com>
2018-05-14 00:09:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,17 +8,10 @@ function logShout(string) {
   var string = "HELLO"
   string.toUpperCase() === string
   console.log(string)
-  }
-function logWhisper(string) {
-  var string = "hello"
-  string.toLowerCase() === string
-  console.log(string)
 }
-
-function sayHiToGrandma(string) {
-var string = "hello"
- if string.toLowerCase() === lowercase
-  console.log("I can't hear you!")
- if string.toUpperCase() === uppercase
-  console.log("YES INDEED!")
+function sayHiToGrandma(string){  
+  if (string.toLowerCase() === string)
+   return "I can't hear you!"
+  if (string.toUpperCase() === string)
+  
 }
